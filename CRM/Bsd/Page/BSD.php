cec5408cc724cdea3370ee579b202754ae53733f
scardinius <scardinius@chords.pl>
2015-09-08 16:33:26 +0200

    Refactoring + first approach to issues #2, #3

--- CRM/Bsd/Page/BSD.php
+++ CRM/Bsd/Page/BSD.php
@@ -3,78 +3,224 @@
 require_once 'CRM/Core/Page.php';
 
 class CRM_Bsd_Page_BSD extends CRM_Core_Page {
+
+  // TODO Lookup
+  private $groupId = 42;
+
+  private $campaign = array();
+
+  private $campaignId = 0;
+
   function run() {
 
-  $param=json_decode (file_get_contents('php://input') );
+    $param = json_decode(file_get_contents('php://input'));
 
     header('HTTP/1.1 503 Men at work');
 
-    echo json_encode (array (
-      "error"=>"not_implemented",
-      "error_description"=>"Please retry later"
-     ));
-    if (!$param)
+    if (!$param) {
       die ("missing POST PARAM");
+    }
 
-    // TODO Lookup
-    $group_id=42;
-    $campaign_id=3;
-
-    $h=$param->cons_hash;
-       //,'api.email.create'=>array('email'=>$h->emails[0]->email,'is_primary'=>1)
-
-    if ($param->action_type == 'share') {
-      CRM_Core_Error::debug_var("api result",$param,true,true);
-      return; 
-    } 
-
-    $contact=array(
-       'sequential' => 1,
-       'contact_type' => 'Individual',
-       'source' => 'speakout '.$param->action_type . ' '.$param->external_id, 
-       'first_name' => $h->firstname
-       ,'last_name'=>$h->lastname
-       ,'email'=>$h->emails[0]->email
-       ,'api.address.create'=>array('postal_code'=>$h->addresses[0]->zip,'is_primary'=>1,'location_type_id'=>1)
-       ,'api.group_contact.create'=>array('group_id'=>$group_id,"status"=> "Pending")
-
-    );
-    if ($param->action_type == 'petition') {
-      $contact['api.activity.create'] = array(
-      'source_contact_id'=>'$value.id'
-      ,'source_record_id' => $campaign_id
-      ,'campaign'=> $campaign_id
-      ,"activity_type_id"=> "Petition"
-      ,"activity_date_time"=> $param->create_dt
-      ,'subject' => $param->action_name
-      ,"location"=> $param->action_technical_type
-      );
+    $this->campaign = $this->getCampaign($param->external_id);
+    if ($this->isValidCampaign($this->campaign)) {
+      $this->campaignId = $this->campaign['id'];
     } else {
       return;
     }
+    
+    switch ($param->action_type) {
+      case 'petition':
+        $this->petition($param);
+        break;
+      
+      case 'share':
+        $this->share($param);
+        break;
+      
+      default:
+        CRM_Core_Error::debug_var('BSD API, Unsupported Action Type', $param->action_type, false, true);
+    }
 
-    CRM_Core_Error::debug_var("contact",$contact,true,true);
-    $r=civicrm_api3('contact','create',$contact);
-
-    if ( 9 == $param->external_id)
-      return; // test campaign for load, no email sent
-
-    CRM_Core_Error::debug_var("api result",$r,true,true);
-    if (!$r["is_error"])
-      $tplid=69;
-      if ($param->external_id == 8) {
-        $tplid=70;
-      }
-      $s=civicrm_api3("speakout","sendconfirm", array(
-        'sequential' => 1,
-        'messageTemplateID' => $tplid,
-        'toEmail' => $h->emails[0]->email,
-         'contact_id' => $r["id"]
-      ));
-    CRM_Core_Error::debug_var("mail",$sr,true,true);
-     
-//    parent::run();
   }
 
+
+  public function petition($param) {
+
+    $contact = $this->createContact($param);
+    $activity = $this->createActivity($param, $contact['id'], 'Petition');
+
+    if ($this->checkIfConfirm($param->external_id)) {
+      $h = $param->cons_hash;
+      $this->sendConfirm($param, $contact, $h->emails[0]->email);
+    }
+
+    CRM_Core_Error::debug_var("BSD PETITION", $param + $activity, false, true);
+
+  }
+
+
+  public function share($param) {
+
+    $contact = $this->createContact($param);
+    $activity = $this->createActivity($param, $contact['id'], 'share');
+    CRM_Core_Error::debug_var("BSD SHARE", $param + $activity, false, true);
+
+  }
+
+
+  /**
+   * Create or update contact
+   *
+   * @param $param
+   *
+   * @return array
+   * @throws CiviCRM_API3_Exception
+   */
+  private function createContact($param) {
+    $h = $param->cons_hash;
+
+    $contact = array(
+      'sequential' => 1,
+      'contact_type' => 'Individual',
+      'first_name' => $h->firstname,
+      'last_name' => $h->lastname,
+      'email' => $h->emails[0]->email,
+    );
+
+    $result = civicrm_api3('Contact', 'get', $contact);
+    if ($result['count'] == 1) {
+      $contact['id'] = $result['values'][0]['id'];
+    } else {
+      $contact['source'] = 'speakout ' . $param->action_type . ' ' . $param->external_id;
+    }
+
+    $apiAddress = 'api.Address.create';
+    $apiGroupContact = 'api.GroupContact.create';
+
+    $contact[$apiAddress] = array(
+      'postal_code' => $h->addresses[0]->zip,
+      'is_primary' => 1,
+      'location_type_id' => 1,
+    );
+    $contact[$apiGroupContact] = array(
+      'group_id' => $this->groupId,
+      'contact_id' => '$value.id',
+      'status' => 'Pending',
+    );
+
+    return civicrm_api3('Contact', 'create', $contact);
+
+  }
+
+
+  /**
+   * Create new activity for contact.
+   *
+   * @param $param
+   * @param $contact_id
+   * @param string $activity_type
+   *
+   * @return array
+   * @throws CiviCRM_API3_Exception
+   */
+  private function createActivity($param, $contact_id, $activity_type = 'Petition') {
+    $activity_type_id = CRM_Core_OptionGroup::getValue('activity_type', $activity_type);
+    $params = array(
+      'source_contact_id' => $contact_id,
+      'source_record_id' => $this->campaignId,
+      'campaign' => $this->campaignId,
+      'activity_type_id' => $activity_type_id,
+      'activity_date_time' => $param->create_dt,
+      'subject' => $param->action_name,
+      'location' => $param->action_technical_type,
+    );
+    return civicrm_api3('Activity', 'create', $params);
+  }
+
+
+  /**
+   * Get campaign by external identifier.
+   *
+   * @param $external_identifier
+   *
+   * @return array
+   * @throws CiviCRM_API3_Exception
+   */
+  private function getCampaign($external_identifier) {
+    if ($external_identifier > 0) {
+      $params = array(
+        'sequential' => 1,
+        'external_identifier' => (int)$external_identifier,
+      );
+      $result = civicrm_api3('Campaign', 'get', $params);
+      if ($result['count'] == 1) {
+        return $result['values'][0];
+      }
+    }
+    return array();
+  }
+  
+
+  /**
+   * Determine whether $campaign table has a valid structure.
+   *
+   * @param $campaign
+   *
+   * @return bool
+   */
+  private function isValidCampaign($campaign) {
+    if (
+      is_array($campaign) &&
+      array_key_exists('id', $campaign) &&
+      $campaign['id'] > 0
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+
+  /**
+   * Check whether this external campaing (SpeakOut ID Campaign) is marked as unsupported (ex. testing campaign).
+   *
+   * @param $external_id
+   *
+   * @return bool
+   */
+  private function checkIfConfirm($external_id) {
+    $notconfirm_external_id = array(
+      9,
+    );
+    return !in_array($external_id, $notconfirm_external_id);
+  }
+
+
+  /**
+   * Send confirmation mail to contact.
+   *
+   * @param $param
+   * @param $contact_result
+   * @param $email
+   *
+   * @return array
+   * @throws CiviCRM_API3_Exception
+   */
+  private function sendConfirm($param, $contact_result, $email) {
+    // todo ???
+    if (!$contact_result['is_error']) {
+      $tplid = 69;
+    }
+    // todo ???
+    if ($param->external_id == 8) {
+      $tplid = 70;
+    }
+    $params = array(
+      'sequential' => 1,
+      'messageTemplateID' => $tplid, // todo retrieve template id from customField (how to do it?)
+      'toEmail' => $email,
+      'contact_id' => $contact_result['id']
+    );
+    return civicrm_api3("Speakout", "sendconfirm", $params);
+  }
 
 }
--- api/v3/Speakout.php
+++ api/v3/Speakout.php
@@ -1,34 +1,35 @@
-<?php 
+<?php
 
 function _civicrm_api3_speakout_sendconfirm_spec(&$params) {
   // TODO a 'clever' default should be introduced
   $params['toEmail']['api.required'] = 1;
   $params['contact_id']['api.required'] = 1;
   $params['messageTemplateID']['api.default'] = 69;
-  $params['from']['api.default'] ='"Xavier - WeMove.EU" <info@wemove.eu>';
+  $params['from']['api.default'] = '"Xavier - WeMove.EU" <info@wemove.eu>';
 }
 
-function civicrm_api3_speakout_sendconfirm ($params) {
-      $query = 'SELECT msg_subject subject, msg_text text, msg_html html, pdf_format_id format
+function civicrm_api3_speakout_sendconfirm($params) {
+  $query = 'SELECT msg_subject subject, msg_text text, msg_html html, pdf_format_id format
                       FROM civicrm_msg_template mt
                       WHERE mt.id = %1 AND mt.is_default = 1';
-      $sqlParams = array(1 => array($params['messageTemplateID'], 'String'));
-    $dao = CRM_Core_DAO::executeQuery($query, $sqlParams);
-    $dao->fetch();
+  $sqlParams = array(1 => array($params['messageTemplateID'], 'String'));
+  $dao = CRM_Core_DAO::executeQuery($query, $sqlParams);
+  $dao->fetch();
 
   if (!$dao->N) {
-   CRM_Core_Error::fatal(ts('No such message template: id=%1.', array(1 => $params['messageTemplateID'])));
+    CRM_Core_Error::fatal(ts('No such message template: id=%1.', array(1 => $params['messageTemplateID'])));
   }
-    $cgid=$params['contact_id'];
-    $hash= sha1(CIVICRM_SITE_KEY.$cgid);
-    $url = CRM_Utils_System::url('civicrm/speakout/confirm',
-          "id=$cgid&hash=$hash&utm_source=civicrm&utm_medium=email&utm_campaign=speakout_confirm",true);
-    $params['subject'] = $dao->subject;
-    $params['text'] = str_replace("#speakout_url_confirm",$url,$dao->text);
-    $params['html'] = str_replace("#speakout_url_confirm",$url,$dao->html);
-    $params['format'] = $dao->format;
-    $dao->free();
+  $cgid = $params['contact_id'];
+  $hash = sha1(CIVICRM_SITE_KEY . $cgid);
+  $url = CRM_Utils_System::url('civicrm/speakout/confirm',
+    "id=$cgid&hash=$hash&utm_source=civicrm&utm_medium=email&utm_campaign=speakout_confirm", true);
+  $params['subject'] = $dao->subject;
+  $params['text'] = str_replace("#speakout_url_confirm", $url, $dao->text);
+  $params['html'] = str_replace("#speakout_url_confirm", $url, $dao->html);
+  $params['format'] = $dao->format;
+  $dao->free();
 
-    $sent = CRM_Utils_Mail::send($params);
-    return civicrm_api3_create_success($sent,$params);
+  $sent = CRM_Utils_Mail::send($params);
+
+  return civicrm_api3_create_success($sent, $params);
 }
