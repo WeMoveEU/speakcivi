9e98f3ae839b4224de7b470947ee5a7a04f93ed6
scardinius <scardinius@chords.pl>
2015-09-16 15:23:35 +0200

    Added tools for testing (1)

--- tools/campaign.php
+++ tools/campaign.php
@@ -1,7 +1,22 @@
 <?php
 
 session_start();
-require_once '../../civicrm.config.php';
+//require_once '../../civicrm.config.php';
+
+$settingsFile = '../civicrm.settings.php';
+define('CIVICRM_SETTINGS_PATH', $settingsFile);
+$error = @include_once( $settingsFile );
+if ( $error == false ) {
+  echo "Could not load the settings file at: {$settingsFile}\n";
+  exit( );
+}
+
+// Load class loader
+global $civicrm_root;
+require_once $civicrm_root . '/CRM/Core/ClassLoader.php';
+CRM_Core_ClassLoader::singleton()->register();
+
+
 require_once 'CRM/Core/Config.php';
 $config = CRM_Core_Config::singleton();
 
