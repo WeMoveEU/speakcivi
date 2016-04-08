8d5c63f3a04f359c1b85481623c7d4dfcca086c5
scardinius <scardinius@chords.pl>
2016-04-08 17:17:26 +0200

    Comment on getTypeId()

--- CRM/Speakcivi/Logic/Activity.php
+++ CRM/Speakcivi/Logic/Activity.php
@@ -87,7 +87,7 @@ class CRM_Speakcivi_Logic_Activity {
    * @param $parentActivityId
    */
   public static function join($contactId, $subject = '', $campaignId = 0, $parentActivityId = 0) {
-    // todo move to construct?
+    // todo change to id of type, then remove getTypeId()
     $activityTypeName = CRM_Core_BAO_Setting::getItem('Speakcivi API Preferences', 'activity_type_join');
     $activityTypeId = self::getTypeId($activityTypeName);
     self::createActivity($contactId, $activityTypeId, $subject, $campaignId, $parentActivityId);
@@ -103,7 +103,7 @@ class CRM_Speakcivi_Logic_Activity {
    * @param $parentActivityId
    */
   public static function leave($contactId, $subject = '', $campaignId = 0, $parentActivityId = 0) {
-    // todo move to construct?
+    // todo change to id of type, then remove getTypeId()
     $activityTypeName = CRM_Core_BAO_Setting::getItem('Speakcivi API Preferences', 'activity_type_leave');
     $activityTypeId = self::getTypeId($activityTypeName);
     self::createActivity($contactId, $activityTypeId, $subject, $campaignId, $parentActivityId);
