8d7a45a64d4cd0923395dedbc7de8e1df93eb27f
scardinius <scardinius@chords.pl>
2016-09-22 13:30:54 +0200

    Add utm_source field for tracking codes #130

--- tools/settings.php
+++ tools/settings.php
@@ -89,6 +89,9 @@ var_dump($result);
 $result = CRM_Core_BAO_Setting::getItem('Speakcivi API Preferences', 'field_contact_campaign');
 var_dump($result);
 
+$result = CRM_Core_BAO_Setting::getItem('Speakcivi API Preferences', 'field_tracking_codes_source');
+var_dump($result);
+
 $result = CRM_Core_BAO_Setting::getItem('Speakcivi API Preferences', 'field_tracking_codes_medium');
 var_dump($result);
 
