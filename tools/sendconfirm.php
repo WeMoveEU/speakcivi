feb9b13b29e05639d4d84ab2b7067bc856db7307
scardinius <scardinius@chords.pl>
2017-01-19 17:42:26 +0100

    Add logging during sending confirmation by script

--- tools/sendconfirm.php
+++ tools/sendconfirm.php
@@ -19,10 +19,13 @@ $config = CRM_Core_Config::singleton();
 
 $path = 'sendconfirm.csv';
 $sheet = file($path);
+
+$log = 'sendconfirm.log';
+$fp = fopen($log, 'a+');
 foreach ($sheet as $id => $line) {
 
   // $email, $contactId, $activityId, $campaignId, $confirmationBlock, $share_utm_source = ''
-  $row = str_getcsv($line, ";", '"');
+  $row = str_getcsv($line, ",", '"');
 
   $params = array(
     'sequential' => 1,
@@ -30,12 +33,15 @@ foreach ($sheet as $id => $line) {
     'contact_id' => $row[1],
     'activity_id' => $row[2],
     'campaign_id' => $row[3],
-    'confirmation_block' => $row[4],
+    'confirmation_block' => (boolean)$row[4],
   );
   if ($row[5]) {
     $params['share_utm_source'] = $row[5];
   }
   $result = civicrm_api3("Speakcivi", "sendconfirm", $params);
-  echo $row[0] . ": " . $result['count'] . "\n";
+  $output = date('Y-m-d H:i:s') . ";" . $row[0] . ";" . $result['is_error'] . ";" . $result['version'] . ";" . $result['count'] . ";" . $result['values'] . "\n";
+  fwrite($fp, $output);
+  usleep(100000);
 }
+fclose($fp);
 echo "Konec\n\n";
