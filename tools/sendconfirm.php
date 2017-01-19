e742a74c152e4a24fcc208f56cc93003781c5f85
scardinius <scardinius@chords.pl>
2017-01-19 17:17:53 +0100

    Send confirmation emails by script

--- tools/sendconfirm.php
+++ tools/sendconfirm.php
@@ -17,46 +17,25 @@ CRM_Core_ClassLoader::singleton()->register();
 require_once 'CRM/Core/Config.php';
 $config = CRM_Core_Config::singleton();
 
+$path = 'sendconfirm.csv';
+$sheet = file($path);
+foreach ($sheet as $id => $line) {
 
-// tests:
-$param = (object)array(
-  'action_name' => 'Testowa kampania',
-  'action_type' => 'petition',
-  'action_technical_type' => 'act2.wemove.eu:petition',
-  'external_id' => 49,
-  'create_dt' => '2016-01-08T11:56:59.617+01:00',
-  'cons_hash' => (object)array(
-    'firstname' => 'Tomasz',
-    'lastname' => 'Pietrzkowski [M]',
-    'emails' => array(
-      0 => (object)array(
-        'email' => 'tomasz.pietrzkowski@chords.pl',
-      )
-    ),
-    'addresses' => array(
-      0 => (object)array(
-        'zip' => '[pl] 02-222',
-      ),
-    ),
-  ),
-  'boolean_collection' => true,
-  'comment' => 'Komentarz do petycji',
-);
+  // $email, $contactId, $activityId, $campaignId, $confirmationBlock, $share_utm_source = ''
+  $row = str_getcsv($line, ";", '"');
 
-$speakcivi = new CRM_Speakcivi_Page_Speakcivi();
-
-$speakcivi->setDefaults();
-$speakcivi->setCountry($param);
-$speakcivi->campaignObj = new CRM_Speakcivi_Logic_Campaign();
-$speakcivi->campaign = $speakcivi->campaignObj->getCampaign($param->external_id);
-$speakcivi->campaign = $speakcivi->campaignObj->setCampaign($param->external_id, $speakcivi->campaign, $param);
-if ($speakcivi->campaignObj->isValidCampaign($speakcivi->campaign)) {
-  $speakcivi->campaignId = $speakcivi->campaign['id'];
-  $speakcivi->campaignObj->customFields = $speakcivi->campaignObj->getCustomFields($speakcivi->campaignId);
-  $speakcivi->locale = $speakcivi->campaignObj->getLanguage();
-} else {
-  echo 'blad :-[';
-  exit;
+  $params = array(
+    'sequential' => 1,
+    'toEmail' => $row[0],
+    'contact_id' => $row[1],
+    'activity_id' => $row[2],
+    'campaign_id' => $row[3],
+    'confirmation_block' => $row[4],
+  );
+  if ($row[5]) {
+    $params['share_utm_source'] = $row[5];
+  }
+  $result = civicrm_api3("Speakcivi", "sendconfirm", $params);
+  echo $row[0] . ": " . $result['count'] . "\n";
 }
-
-$speakcivi->petition($param);
+echo "Konec\n\n";
