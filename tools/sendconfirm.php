5883c6e733244807731815a7409dbbfed11e02c6
scardinius <scardinius@chords.pl>
2017-01-20 12:46:07 +0100

    Fix resending confirmation emails script

--- tools/sendconfirm.php
+++ tools/sendconfirm.php
@@ -27,14 +27,18 @@ foreach ($sheet as $id => $line) {
 
   // $email, $contactId, $activityId, $campaignId, $confirmationBlock, $share_utm_source = ''
   $row = str_getcsv($line, ",", '"');
-
+  if ($row[4] == 'TRUE') {
+    $confirmationBlock = true;
+  } else {
+    $confirmationBlock = false;
+  }
   $params = array(
     'sequential' => 1,
     'toEmail' => $row[0],
     'contact_id' => $row[1],
     'activity_id' => $row[2],
     'campaign_id' => $row[3],
-    'confirmation_block' => (boolean)$row[4],
+    'confirmation_block' => $confirmationBlock,
   );
   if (key_exists(5, $row) && $row[5]) {
     $params['share_utm_source'] = $row[5];
@@ -42,7 +46,7 @@ foreach ($sheet as $id => $line) {
   $result = civicrm_api3("Speakcivi", "sendconfirm", $params);
   $output = '"' . date('Y-m-d H:i:s') . "\";\"" . $row[0] . "\";" . $result['is_error'] . ";" . $result['version'] . ";" . $result['count'] . ";" . $result['values'] . "\n";
   fwrite($fp, $output);
-  usleep(100000);
+  usleep(50000);
 }
 fclose($fp);
 echo "Konec\n\n";
