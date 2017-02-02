c250cb71ed4c24bb5372877c715ed8534d92eb31
scardinius <scardinius@chords.pl>
2017-02-02 10:11:37 +0100

    Add more fields to bash script #142

--- tools/run.sh
+++ tools/run.sh
@@ -3,10 +3,12 @@
 ENDPOINT=$1
 DB=$2
 USR=$3
-PSW=$4
 
 TEST_DATETIME=$(date +%Y-%m-%d\ %H:%M:%S)
 TEST_HASH=$(date +%s)
+
+echo "db password:"
+read PSW
 
 echo "1. Sign petition at ${TEST_DATETIME}"
 curl $ENDPOINT -d "{
@@ -16,6 +18,8 @@ curl $ENDPOINT -d "{
 \"action_name\":\"${TEST_HASH}\",
 \"external_id\":\"10007\",
 \"cons_hash\":{\"firstname\":\"Tomasz\",\"lastname\":\"Pietrzkowski\",\"emails\":[{\"email\":\"scardinius@chords.pl\"}],\"addresses\":[{\"zip\":\"[pl] 01-111\"}]}
+\"source\":{\"source\":\"source-script\",\"medium\":\"medium-script\",\"campaign\":\"campaign-script\"},
+\"metadata\":{\"tracking_codes\":{\"source\":\"source-tracking\",\"medium\":\"medium-tracking\",\"campaign\":\"campaign-tracking\",\"content\":\"content-tracking\"}}
 }"
 
 PETITION_QUERY="SELECT count(id) AS count_of_petition1
