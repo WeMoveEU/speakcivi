5f7e29da4ed8248cc2dab38772b251971badc900
scardinius <scardinius@chords.pl>
2017-02-02 11:23:18 +0100

    Update bash script, deleting data #142

--- tools/run.sh
+++ tools/run.sh
@@ -7,10 +7,9 @@ USR=$3
 TEST_DATETIME=$(date +%Y-%m-%d\ %H:%M:%S)
 TEST_HASH=$(date +%s)
 
-echo "db password:"
-read PSW
+read -s -p "db password: " PSW
 
-echo "1. Sign petition at ${TEST_DATETIME}"
+echo "1. Sign petition, current member at ${TEST_DATETIME}"
 curl $ENDPOINT -d "{
 \"action_type\":\"petition\",
 \"action_technical_type\":\"you.wemove.eu:petition\",
@@ -22,9 +21,20 @@ curl $ENDPOINT -d "{
 \"metadata\":{\"tracking_codes\":{\"source\":\"source-tracking\",\"medium\":\"medium-tracking\",\"campaign\":\"campaign-tracking\",\"content\":\"content-tracking\"}}
 }"
 
-PETITION_QUERY="SELECT count(id) AS count_of_petition1
-FROM civicrm_activity
+PETITION_QUERY="SELECT a.id, a.activity_date_time, a.subject, a.campaign_id, s.source_27, s.media_28, s.campaign_26
+FROM civicrm_activity a
+  JOIN civicrm_value_action_source_4 s ON s.entity_id = a.id
 WHERE activity_type_id = 32 AND subject = '${TEST_HASH}'"
 
 mysql $DB -u $USR -p$PSW -e "$PETITION_QUERY"
 echo -e "\n"
+
+
+# delete
+read -p "delete testing data? (y/n): " DROP
+if [ "$DROP" = "y" ] ; then
+  echo "deleting data..."
+  echo "delete activity for current member..."
+  mysql $DB -u $USR -p$PSW -e "DELETE FROM civicrm_activity WHERE activity_type_id = 32 AND subject = '${TEST_HASH}'"
+  echo -e "\n"
+fi
