c4755ee19576bbc399794a2022664deacdfb8b6a
scardinius <scardinius@chords.pl>
2017-05-18 16:33:26 +0200

    Refactor cache for campaign #147

--- CRM/Speakcivi/Logic/Cache.php
+++ CRM/Speakcivi/Logic/Cache.php
@@ -2,6 +2,8 @@
 
 class CRM_Speakcivi_Logic_Cache {
 
+  const KEY_PREFIX = 'speakcivi-cache-';
+
   private static $dateFormat = 'YmdHis';
 
   public static function campaign($param) {
@@ -24,6 +26,53 @@ class CRM_Speakcivi_Logic_Cache {
     return $cacheCampaign;
   }
 
+
+  /**
+   * Get array cache for given object.
+   *
+   * @param string $type
+   * @param int $id
+   *
+   * @return mixed|null
+   */
+  protected static function get($type, $id) {
+    $key = self::KEY_PREFIX . $type . $id;
+    $cache = Civi::cache()->get($key);
+    if (!isset($cache)) {
+      return NULL;
+    }
+    if (self::isOld($cache['timestamp'])) {
+      Civi::cache()->delete($key);
+      return NULL;
+    }
+    return $cache;
+  }
+
+
+  /**
+   * Set array cache for given object.
+   *
+   * @param string $type
+   * @param int $id
+   * @param mixed $object
+   */
+  protected  static function set($type, $id, $object) {
+    $key = self::KEY_PREFIX . $type . $id;
+    $value = array(
+      $type => $object,
+      'timestamp' => date(self::$dateFormat),
+    );
+    Civi::cache()->set($key, $value);
+  }
+
+
+  /**
+   * Check if cache is old.
+   *
+   * @param string $timestamp
+   *
+   * @return bool
+   */
   private static function isOld($timestamp) {
     $old = DateTime::createFromFormat(self::$dateFormat, $timestamp)
       ->modify('+1 hour')
