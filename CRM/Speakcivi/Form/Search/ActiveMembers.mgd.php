bd4a28c1d49dd5ebd313ea7ef0ad541f6cab2b2f
scardinius <scardinius@chords.pl>
2017-06-01 15:30:00 +0200

    Add Active Members custom search

---
+++ CRM/Speakcivi/Form/Search/ActiveMembers.mgd.php
@@ -0,0 +1,19 @@
+<?php
+// This file declares a managed database record of type "CustomSearch".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Speakcivi_Form_Search_ActiveMembers',
+    'entity' => 'CustomSearch',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'ActiveMembers',
+      'description' => 'ActiveMembers (eu.wemove.speakcivi)',
+      'class_name' => 'CRM_Speakcivi_Form_Search_ActiveMembers',
+    ),
+  ),
+);
