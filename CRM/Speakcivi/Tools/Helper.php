dfbe18689445b88592b7adf49145ddb22418bf2f
scardinius <scardinius@chords.pl>
2017-06-01 11:05:14 +0200

    Remove emoticons from strings

--- CRM/Speakcivi/Tools/Helper.php
+++ CRM/Speakcivi/Tools/Helper.php
@@ -11,7 +11,30 @@ class CRM_Speakcivi_Tools_Helper {
    * @return mixed
    */
   public static function cleanUnicodeChars($string) {
-    return preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $string);
+    $regexEmoticons = '/[\x00-\x1F\x80-\xFF]/';
+    $clean_text = preg_replace($regexEmoticons, '', $string);
+
+    // Match Emoticons
+    $regexEmoticons = '/[\x{1F600}-\x{1F64F}]/u';
+    $clean_text = preg_replace($regexEmoticons, '', $clean_text);
+
+    // Match Miscellaneous Symbols and Pictographs
+    $regexSymbols = '/[\x{1F300}-\x{1F5FF}]/u';
+    $clean_text = preg_replace($regexSymbols, '', $clean_text);
+
+    // Match Transport And Map Symbols
+    $regexTransport = '/[\x{1F680}-\x{1F6FF}]/u';
+    $clean_text = preg_replace($regexTransport, '', $clean_text);
+
+    // Match Miscellaneous Symbols
+    $regexMisc = '/[\x{2600}-\x{26FF}]/u';
+    $clean_text = preg_replace($regexMisc, '', $clean_text);
+
+    // Match Dingbats
+    $regexDingbats = '/[\x{2700}-\x{27BF}]/u';
+    $clean_text = preg_replace($regexDingbats, '', $clean_text);
+
+    return $clean_text;
   }
 
 
