4125e24c232c774ff46c466071b51a9458cb1b13
scardinius <scardinius@chords.pl>
2017-06-01 13:47:52 +0200

    Simplify method for cleaning unicode chars

--- CRM/Speakcivi/Tools/Helper.php
+++ CRM/Speakcivi/Tools/Helper.php
@@ -11,29 +11,15 @@ class CRM_Speakcivi_Tools_Helper {
    * @return mixed
    */
   public static function cleanUnicodeChars($string) {
-    $regexEmoticons = '/[\x00-\x1F\x80-\xFF]/';
-    $clean_text = preg_replace($regexEmoticons, '', $string);
-
-    // Match Emoticons
-    $regexEmoticons = '/[\x{1F600}-\x{1F64F}]/u';
-    $clean_text = preg_replace($regexEmoticons, '', $clean_text);
-
-    // Match Miscellaneous Symbols and Pictographs
-    $regexSymbols = '/[\x{1F300}-\x{1F5FF}]/u';
-    $clean_text = preg_replace($regexSymbols, '', $clean_text);
-
-    // Match Transport And Map Symbols
-    $regexTransport = '/[\x{1F680}-\x{1F6FF}]/u';
-    $clean_text = preg_replace($regexTransport, '', $clean_text);
-
-    // Match Miscellaneous Symbols
-    $regexMisc = '/[\x{2600}-\x{26FF}]/u';
-    $clean_text = preg_replace($regexMisc, '', $clean_text);
-
-    // Match Dingbats
-    $regexDingbats = '/[\x{2700}-\x{27BF}]/u';
-    $clean_text = preg_replace($regexDingbats, '', $clean_text);
-    return $clean_text;
+    $forbiddenRegex = '/['
+      . '\x00-\x1F\x80-\xFF'
+      . '\x{1F600}-\x{1F64F}'  // Match Emoticons
+      . '\x{1F300}-\x{1F5FF}'  // Match Miscellaneous Symbols and Pictographs
+      . '\x{1F680}-\x{1F6FF}'  // Match Transport And Map Symbols
+      . '\x{2600}-\x{26FF}'  // Match Miscellaneous Symbols
+      . '\x{2700}-\x{27BF}'  // Match Dingbats
+      . ']/u';
+    return preg_replace($forbiddenRegex, '', $string);
   }
 
 
