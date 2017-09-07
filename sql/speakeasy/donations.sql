fc97b2c1bd88c86dbd51975469663921916ebda6
xavier dutoit <xavier@sydesy.com>
2017-09-07 13:07:35 +0200

    include 'in progress' in the donations

--- sql/speakeasy/donations.sql
+++ sql/speakeasy/donations.sql
@@ -3,7 +3,7 @@ INSERT INTO speakeasy_petition_metrics (activity, campaign_id, status, npeople)
   SELECT
     'donation', campaign_id, currency, count(id) donations_count
   FROM civicrm_contribution
-  WHERE contribution_status_id = 1 AND is_test = 0 AND campaign_id IS NOT NULL
+  WHERE contribution_status_id in (1,5) AND is_test = 0 AND campaign_id IS NOT NULL
   GROUP BY campaign_id, currency;
 
 -- Donations amount
@@ -11,7 +11,7 @@ INSERT INTO speakeasy_petition_metrics (activity, campaign_id, status, npeople)
   SELECT
     'donation_amount', campaign_id, currency, sum(total_amount)
   FROM civicrm_contribution
-  WHERE contribution_status_id = 1 AND is_test = 0 AND campaign_id IS NOT NULL
+  WHERE contribution_status_id in( 1, 5) AND is_test = 0 AND campaign_id IS NOT NULL
   GROUP BY campaign_id, currency;
 
 -- Donations count on pending status (like SEPA)
