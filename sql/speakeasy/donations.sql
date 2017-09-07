8de56fae03c432f30705c40fd13563d6b4b756e5
scardinius <scardinius@chords.pl>
2017-09-07 12:41:31 +0200

    Add donation on pending status metric

--- sql/speakeasy/donations.sql
+++ sql/speakeasy/donations.sql
@@ -14,6 +14,22 @@ INSERT INTO speakeasy_petition_metrics (activity, campaign_id, status, npeople)
   WHERE contribution_status_id = 1 AND is_test = 0 AND campaign_id IS NOT NULL
   GROUP BY campaign_id, currency;
 
+-- Donations count on pending status (like SEPA)
+INSERT INTO speakeasy_petition_metrics (activity, campaign_id, status, npeople)
+  SELECT
+    'donation_pending', campaign_id, currency, count(id) donations_count
+  FROM civicrm_contribution
+  WHERE contribution_status_id = 2 AND is_test = 0 AND campaign_id IS NOT NULL
+  GROUP BY campaign_id, currency;
+
+-- Donations amount on pending status (like SEPA)
+INSERT INTO speakeasy_petition_metrics (activity, campaign_id, status, npeople)
+  SELECT
+    'donation_pending_amount', campaign_id, currency, sum(total_amount)
+  FROM civicrm_contribution
+  WHERE contribution_status_id = 2 AND is_test = 0 AND campaign_id IS NOT NULL
+  GROUP BY campaign_id, currency;
+
 -- Recurring donations
 INSERT INTO speakeasy_petition_metrics (activity, campaign_id, status, npeople)
   SELECT
