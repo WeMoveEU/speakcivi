857125f6844e44cf908872f45064c4ee3ce028aa
Romain Thouvenin <romain@wemove.eu>
2017-09-13 12:32:58 +0200

    Exclude draft mailings from recipient counters

--- sql/speakeasy/unique_recipients.sql
+++ sql/speakeasy/unique_recipients.sql
@@ -1,6 +1,7 @@
 INSERT INTO speakeasy_petition_metrics (activity, campaign_id, npeople) SELECT
   'unique_recipient', cp.id, count(DISTINCT contact_id)
 FROM civicrm_mailing m
-  JOIN civicrm_mailing_recipients r ON r.mailing_id = m.id AND m.name NOT LIKE '%-Reminder-%'
+  JOIN civicrm_mailing_recipients r ON r.mailing_id = m.id
   JOIN civicrm_campaign cp ON cp.id = m.campaign_id
+  WHERE m.scheduled_id IS NOT NULL AND m.name NOT LIKE '%-Reminder-%'
 GROUP BY campaign_id;
