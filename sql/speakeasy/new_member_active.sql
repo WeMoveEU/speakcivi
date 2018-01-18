87d79c718a7b999b8f01270c1daecca1320c5a83
Romain Thouvenin <romain@wemove.eu>
2018-01-18 15:39:20 +0100

    Use 3m definition instead of 1m for new active members metric

--- sql/speakeasy/new_member_active.sql
+++ sql/speakeasy/new_member_active.sql
@@ -9,13 +9,13 @@ INSERT INTO speakeasy_petition_metrics (activity, campaign_id, npeople)
   WHERE a.status_id=9 
     AND a_act.activity_type_id IN (6, 28, 32)
     AND a_act.activity_date_time >= DATE_ADD(a.activity_date_time, INTERVAL 1 DAY)
-    AND a_act.activity_date_time >= DATE_ADD(NOW(), INTERVAL -1 MONTH)
+    AND a_act.activity_date_time >= DATE_ADD(NOW(), INTERVAL -3 MONTH)
   GROUP BY a.campaign_id;
 
--- Number of new people who were active 2 months later
+-- Number of new people who were active 3 months later
 INSERT INTO speakeasy_petition_metrics (activity, campaign_id, npeople)
   SELECT 
-    'new_active_2mlater', a.campaign_id, COUNT(DISTINCT ac.contact_id) 
+    'new_active_3mlater', a.campaign_id, COUNT(DISTINCT ac.contact_id) 
   FROM civicrm_activity a 
   JOIN civicrm_activity_contact ac ON ac.activity_id=a.id 
   JOIN civicrm_activity_contact ac_act ON ac.contact_id=ac_act.contact_id
@@ -23,6 +23,6 @@ INSERT INTO speakeasy_petition_metrics (activity, campaign_id, npeople)
   WHERE a.status_id=9 
     AND a_act.activity_type_id IN (6, 28, 32)
     AND (a_act.activity_date_time 
-      BETWEEN DATE_ADD(a.activity_date_time, INTERVAL 1 MONTH)
-          AND DATE_ADD(a.activity_date_time, INTERVAL 2 MONTH))
+      BETWEEN DATE_ADD(a.activity_date_time, INTERVAL 1 DAY)
+          AND DATE_ADD(a.activity_date_time, INTERVAL 3 MONTH))
   GROUP BY a.campaign_id;
