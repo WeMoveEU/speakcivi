1180a5aa5dc3d54a827a5ba252c6fd8ef9a4ac53
Romain Thouvenin <romain@wemove.eu>
2018-02-15 11:10:19 +0100

    Read speakeasy unsub from 10d timebox

--- sql/speakeasy/unsubs.sql
+++ sql/speakeasy/unsubs.sql
@@ -2,5 +2,5 @@ INSERT INTO speakeasy_petition_metrics (activity, campaign_id, npeople)
   SELECT
     'unsub', campaign_id, sum(r.value)
   FROM civicrm_mailing m
-  LEFT JOIN data_mailing_counter r ON r.mailing_id = m.id AND r.counter = 'unsubs' AND r.timebox = 144000
+  LEFT JOIN data_mailing_counter r ON r.mailing_id = m.id AND r.counter = 'unsubs' AND r.timebox = 14400
   GROUP BY campaign_id;
