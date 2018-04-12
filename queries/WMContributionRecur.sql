b54bc86e104b65541a813531414bc1d5b6bcf672
Xavier Dutoit <xavier@wemove.eu>
2018-04-12 10:29:50 +0200

    Fix column grouping in contrib query

--- queries/WMContributionRecur.sql
+++ queries/WMContributionRecur.sql
@@ -2,25 +2,25 @@ select
   R.id,
   co.first_name,
   R.contact_id,
-  utm_campaign_33 as camp, 
-  utm_source_30 as source,
-  utm_medium_31 as medium,
-  utm_content_32 as content,
+  min(utm_campaign_33) as camp, 
+  min(utm_source_30) as sorce,
+  min(utm_medium_31) as medium,
+  min(utm_content_32) as content,
   create_date as date, 
   R.cancel_date,
   R.currency, 
   pp.name as processor, 
-  status.name as status, 
-  M.status as sepa_status, 
+  min(status.name) as status, 
+  min(M.status) as sepa_status, 
   frequency_unit as frequency, 
   amount,
   SUM(IF(c.contribution_status_id = 1,1,0)) AS nb,
   SUM(IF(c.contribution_status_id = 1,c.total_amount,0)) AS total_amount,
-  ctry.iso_code as country,
+  min(ctry.iso_code) as country,
   co.preferred_language as lang,
   co.created_date as contact_since,
-  ab.ab_testing_42 as ab_test,
-  ab.ab_variant_43 as ab_variant
+  min(ab.ab_testing_42) as ab_test,
+  min(ab.ab_variant_43) as ab_variant
 from civicrm_contribution_recur  as R 
 join civicrm_contact co on R.contact_id=co.id 
 join civicrm_payment_processor pp on payment_processor_id=pp.id 
@@ -33,4 +33,5 @@ left join civicrm_address a on a.contact_id=c.contact_id and is_primary=1
 left join civicrm_country ctry on a.country_id= ctry.id
 where R.is_test=0 and c.is_test=0
 group by R.id
-order by date desc;
+order by date desc
+limit 10;
