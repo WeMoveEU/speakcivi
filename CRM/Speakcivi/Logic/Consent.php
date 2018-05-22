64851e56be9d3f9c3ad2c4f384249a4f468e118f
scardinius <scardinius@chords.pl>
2018-05-22 14:11:09 +0200

    Fix date of DPA activity properly

--- CRM/Speakcivi/Logic/Consent.php
+++ CRM/Speakcivi/Logic/Consent.php
@@ -26,7 +26,7 @@ class CRM_Speakcivi_Logic_Consent {
         $c->version = $consentVersion;
         $c->language = $consentLanguage;
         $c->date = $cd->format('Y-m-d');
-        $c->createDate = $cd->format('Y-m-d H:i:s');
+        $c->createDate = $param->create_dt;
         $c->level = $consent->consent_level;
         $c->method = $consent->consent_method;
         $c->methodOption = $consent->consent_method_option;
