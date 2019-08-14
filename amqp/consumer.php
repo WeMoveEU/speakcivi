d15d87fb5cc7e70db09411f300c7886f63686a98
Romain Thouvenin <romain@wemove.eu>
2019-08-14 10:00:25 +0200

    Connect to RabbitMQ with SSL

--- amqp/consumer.php
+++ amqp/consumer.php
@@ -23,7 +23,7 @@ $civicrm_config = CRM_Core_Config::singleton();
 CRM_Utils_System::loadBootStrap(array('uid' => 1), TRUE, TRUE, $civicrm_root);
 
 require_once __DIR__ . '/vendor/autoload.php';
-use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Connection\AMQPSSLConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 use PhpAmqpLib\Wire\AMQPTable;
 
@@ -54,9 +54,14 @@ $error_queue = $arguments['e'];
 $retry_exchange = $arguments['r'];
 
 function connect() {
-  return new AMQPStreamConnection(
+  return new AMQPSSLConnection(
     CIVICRM_AMQP_HOST, CIVICRM_AMQP_PORT,
-    CIVICRM_AMQP_USER, CIVICRM_AMQP_PASSWORD, CIVICRM_AMQP_VHOST);
+    CIVICRM_AMQP_USER, CIVICRM_AMQP_PASSWORD, CIVICRM_AMQP_VHOST,
+    array(
+      'local_cert' => CIVICRM_SSL_CERT,
+      'local_pk' => CIVICRM_SSL_KEY,
+    )
+  );
 }
 
 /**
