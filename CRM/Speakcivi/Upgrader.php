7c54cbe01b81b4afaf7ec2a1739c029f3efc32c8
Romain Thouvenin <romain@wemove.eu>
2019-09-25 15:03:15 +0200

    Language conversion block

--- CRM/Speakcivi/Upgrader.php
+++ CRM/Speakcivi/Upgrader.php
@@ -5,6 +5,33 @@
  */
 class CRM_Speakcivi_Upgrader extends CRM_Speakcivi_Upgrader_Base {
 
+  public function createCustomFields() {
+    $result = civicrm_api3('CustomGroup', 'get', [ 'name' => "speakout_integration" ]);
+    if ($result['count'] == 0) {
+      civicrm_api3('CustomGroup', 'create', [
+        'title' => "Speakout integration",
+        'extends' => "Campaign",
+        'name' => "speakout_integration",
+        'table_name' => "civicrm_value_speakout_integration_2",
+      ]);
+    }
+
+    $result = civicrm_api3('CustomField', 'get', [ 'name' => "language_form_url" ]);
+    if ($result['count'] == 0) {
+      civicrm_api3('CustomField', 'create', [
+        'custom_group_id' => "speakout_integration",
+        'label' => "Language conversion form URL",
+        'name' => "language_form_url",
+        'column_name' => "language_form_url",
+        'data_type' => "String",
+        'html_type' => "Text",
+        'is_view' => 0,
+        'is_searchable' => 0,
+      ]);
+    }
+  }
+
+
   /**
    * Upgrade to version 1.1 (Clean up Join and Leave activities)
    *
---
+++ templates/CRM/Speakcivi/Page/LanguageBlock.html.tpl
@@ -0,0 +1,13 @@
+<div style="font-size: 16px; font-family: Helvetica, Arial, sans-serif; ">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td>
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td align="center" style="-webkit-border-radius: 3px; -moz-border-radius: 3px; border-radius: 3px;" bgcolor="#07B1B2"><a href="{$language_form_url}?cid={$contact.id}&cs={$contact.checksum}&utm_source={$share_utm_source}&utm_medium=email&utm_campaign={$utm_campaign}" target="_blank" style="color: #ffffff; text-decoration: none; -webkit-border-radius: 3px; -moz-border-radius: 3px; border-radius: 3px; padding: 12px 18px; border: 1px solid #07B1B2; display: inline-block;">{$language_button}</a></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</div>
