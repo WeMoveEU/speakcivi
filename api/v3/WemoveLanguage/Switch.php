ff7a262c65ed706048644d460f34b32c4ce3f1e8
scardinius <scardinius@chords.pl>
2020-04-21 12:43:15 +0200

    Add empty api action #switch-language

---
+++ api/v3/WemoveLanguage/Switch.php
@@ -0,0 +1,53 @@
+<?php
+
+function _civicrm_api3_wemove_language_switch_spec(&$spec) {
+  $spec['hash'] = [
+    'name' => 'hash',
+    'title' => ts('Contact hash'),
+    'description' => ts('Contact hash'),
+    'type' => CRM_Utils_Type::T_STRING,
+    'api.required' => 1,
+    'api.default' => '',
+  ];
+  $spec['language'] = [
+    'name' => 'language',
+    'title' => ts('Language in locale format'),
+    'description' => ts('Language in locale format (fr_FR)'),
+    'type' => CRM_Utils_Type::T_STRING,
+    'api.required' => 1,
+    'api.default' => '',
+  ];
+}
+
+/**
+ * Switch the contact to new preferred language:
+ *
+ * 1. Set preferred_language
+ * 2. remove from current language groups
+ * 3. add to new language group
+ * 4. create "self-care" activity
+ *
+ * @param $params
+ *
+ * @return array
+ */
+function civicrm_api3_wemove_language_switch(&$params) {
+  $start = microtime(TRUE);
+  $hash = $params['hash'];
+  $language = $params['language'];
+
+  // todo identify contact by hash (civicrm_contact.hash)
+  // todo validate language
+  // todo Set preferred_language
+  // todo remove from current language groups
+  // todo add to new language group
+  // todo create "self-care" activity
+
+  $values = [
+    $hash,
+    $language,
+  ];
+
+  $extraReturnValues = ['time' => microtime(TRUE) - $start];
+  return civicrm_api3_create_success($values, $params, 'wemove_language', 'switch', $blank, $extraReturnValues);
+}
