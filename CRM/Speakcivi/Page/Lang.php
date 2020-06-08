93ce276a57b26163a8a9a30fc2b6f142c19365e3
Michal Mach <michal@wemove.eu>
2020-06-08 16:00:32 +0200

    let's go with language-change

--- CRM/Speakcivi/Page/Lang.php
+++ CRM/Speakcivi/Page/Lang.php
@@ -4,7 +4,7 @@ require_once 'CRM/Core/Page.php';
 
 class CRM_Speakcivi_Page_Lang extends CRM_Speakcivi_Page_Post {
 
-  private $urlLanguageForm = '/language-switch';
+  private $urlLanguageForm = '/language-change';
 
   private $thankYouPage = 'language-change-confirmation';
 
