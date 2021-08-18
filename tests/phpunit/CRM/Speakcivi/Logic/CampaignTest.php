d3847405d59859cb6db8d9ca7bc82166193dcdb2
Romain Thouvenin <romain@wemove.eu>
2021-08-18 16:10:36 +0200

    Unit tests with GH workflow (#175)
    * Boiler-plate code for unit tests
    
    * Tests for retrieving campaigns from cache
    
    * GH workflow to run unit tests

---
+++ tests/phpunit/CRM/Speakcivi/Logic/CampaignTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
+/**
+ * FIXME - Add test description.
+ *
+ * Tips:
+ *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
+ *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
+ *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
+ *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
+ *    If this test needs to manipulate schema or truncate tables, then either:
+ *       a. Do all that using setupHeadless() and Civi\Test.
+ *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ *
+ * @group headless
+ */
+class CRM_Speakcivi_Logic_CampaignTest extends \PHPUnit\Framework\TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+
+  public function setUpHeadless() {
+    // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+    // See: https://docs.civicrm.org/dev/en/latest/testing/phpunit/#civitest
+    return \Civi\Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
+  public function setUp() : void {
+    parent::setUp();
+    $this->externalId = 42;
+    $campaign_result = civicrm_api3('Campaign', 'create', [
+      'campaign_type_id' => 1, 'title' => 'Transient campaign', 'external_identifier' => $this->externalId
+    ]);
+    $this->campaignId = $campaign_result['id'];
+  }
+
+  public function testContructRetrievesExistingCampaign() {
+    $campLogic = new CRM_Speakcivi_Logic_Campaign($this->campaignId);
+    $this->assertNotNull($campLogic->campaign);
+    $this->assertEquals($this->campaignId, $campLogic->campaign['id']);
+  }
+
+  public function testCanReceiveCampaignByExternalId() {
+    $campLogic = new CRM_Speakcivi_Logic_Campaign();
+    $campLogic->campaign = CRM_Speakcivi_Logic_Cache_Campaign::getCampaignByExternalId($this->externalId, 'whatever');
+    $this->assertNotNull($campLogic->campaign);
+    $this->assertEquals($this->campaignId, $campLogic->campaign['id']);
+  }
+
+}
