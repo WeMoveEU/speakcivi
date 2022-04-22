034c7ed6f4c9da8ad71f7f3dc8c9bd0e7741d367
scardinius <scardinius@chords.pl>
2022-04-22 15:13:19 +0200

    Revert "Determine activity status for Survey type (only Completed) #178"
    This reverts commit aaf6d202e891b678340a71f9aea7ea40a5f78779.
    There was a mistake / unclarity in the specifications: surveys don't need special treatment for how the status is determined. You can keep the activity status that's determined based on consent status.

--- CRM/Speakcivi/Page/Speakcivi.php
+++ CRM/Speakcivi/Page/Speakcivi.php
@@ -189,7 +189,7 @@ class CRM_Speakcivi_Page_Speakcivi extends CRM_Core_Page {
   public function processAction($param, $noMember, $activityType) {
     $targetGroupId = $this->determineGroupId();
     $contact = $this->createContact($param, $targetGroupId);
-    $activityStatus = $this->determineActivityStatus($contact, $targetGroupId, $param);
+    $activityStatus = $this->determineActivityStatus($contact, $targetGroupId);
     $activity = $this->createActivity($param, $contact['id'], $activityType, $activityStatus);
 
     if ($this->newContact) {
@@ -536,10 +536,7 @@ class CRM_Speakcivi_Page_Speakcivi extends CRM_Core_Page {
   /**
    * Determine which status to give to the activity
    */
-  public function determineActivityStatus($contact, $targetGroupId, $param): string {
-    if ($this->isPoll($param->action_technical_type)) {
-      return 'Completed';
-    }
+  public function determineActivityStatus($contact, $targetGroupId) {
     switch ($this->consentStatus) {
       case CRM_Speakcivi_Logic_Consent::STATUS_ACCEPTED:
         if ($this->addJoinActivity) {
